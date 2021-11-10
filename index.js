2c02a340fd185294af0c9afc5651e55d910446bd
edpackard <edpackard81@gmail.com>
2021-11-10 10:13:59 +0000

    Update add note function with checks and default values

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ let notes = [
   }
 ]
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map( n => n.id)) : 0
+  return maxId + 1
+}
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>')
 })
@@ -44,9 +49,17 @@ app.get('/api/notes/:id', (request, response) => {
 })
 
 app.post('/api/notes', (request, response) => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map( n => n.id)) : 0
-  const note = request.body
-  note.id = maxId + 1
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({error: 'content missing'})
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
   notes = notes.concat(note)
   response.json(note)
 } )
