ecd7df5bc63375cb18096d4634015c4887aa8af0
edpackard <edpackard81@gmail.com>
2021-11-10 00:16:01 +0000

    Add status code 404 & error message if resource does not exist

--- index.js
+++ index.js
@@ -33,7 +33,12 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
-  response.json(note)
+  if (note) {
+    response.json(note)
+  } else {
+    response.statusMessage = 'Resource does not exist'
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
