998eeeac9d60637d427afeb986f9cef14e1a02c8
edpackard <edpackard81@gmail.com>
2021-11-11 10:27:32 +0000

    Update get note by id function to connect to DB + error handling

--- index.js
+++ index.js
@@ -52,31 +52,36 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.statusMessage = 'Resource does not exist'
-    response.status(404).end()
-  }
+  Note.findById(request.params.id).then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => {
+    console.log(error)
+    response.status(400).send({ error: 'malformatted id'})
+  })
 })
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
-  if (!body.content) {
+  
+  if (body.content === undefined) {
     return response.status(400).json({error: 'content missing'})
   }
 
-  const note = {
+  const note = new Note ({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
-  notes = notes.concat(note)
-  response.json(note)
-} )
+  })
+
+  note.save().then(savedNote => {
+  response.json(savedNote)
+})
+})
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
