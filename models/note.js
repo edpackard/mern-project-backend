4bfa34957be6e10fc1b7a8e3a75ddf3021e7ea65
edpackard <edpackard81@gmail.com>
2021-11-11 16:41:46 +0000

    Update validation for note post route

--- models/note.js
+++ models/note.js
@@ -13,8 +13,8 @@ mongoose.connect(url)
   })
 
   const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
+    content: { type: String, minLength: 5, required: true },
+    date: { type: Date, required: true },
     important: Boolean,
   })
   
