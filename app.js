546a7dce50b2a047e93d6420003779407ff7affb
edpackard <edpackard81@gmail.com>
2021-11-11 19:48:36 +0000

    Restructure project to reflect best practices

---
+++ app.js
@@ -0,0 +1,28 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const notesRouter = require("./controllers/notes");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+logger.info("Connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB:", error.message);
+  });
+
+app.use(express.json()); // JSON parser takes JSON data of req and turns it into JS object attachd to req.body
+app.use(middleware.requestLogger);
+
+app.use("/api/notes", notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
